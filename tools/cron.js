1af59f472e9ab4e1c985ea6f3a39d1c01bb15d09
Arielfang131 <75560708+Arielfang131@users.noreply.github.com>
2021-06-09 19:53:10 +0800

    modified schedule

--- tools/cron.js
+++ tools/cron.js
@@ -294,7 +294,7 @@ async function getNegativeInfo () {
     console.log(negativeInfo);
 }
 
-cron.schedule("00 10 * * *", async () => {
+cron.schedule("00 12 * * *", async () => {
     console.log("testEveryOneHour");
     console.log("========================================");
     await getPtt();
